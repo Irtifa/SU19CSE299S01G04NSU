eeca0457b972471dcfd9908ce176b04dbc80583f
Irtifa <irtifa.sarwath@northsouth.edu>
2019-07-15 00:29:22 +0600

    Feature : connection with mysql issue #11

--- Project Code/connection.php
+++ Project Code/connection.php
@@ -2,3 +2,13 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,"table" );
+// Check connection
+if (!$conn) {
+die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+
+?>
