0f6bbe038b751f7f2348cd62d6cb27318ded4851
Irtifa <irtifa.sarwath@northsouth.edu>
2019-07-15 01:10:03 +0600

    Feature : Creating registration form with php  #11
    Issue #11

--- Project Code/registration.php
+++ Project Code/registration.php
@@ -6,3 +6,11 @@ $link = mysqli_connect("localhost", "root", "", "database");
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+// Escape user inputs for security
+$yourname = mysqli_real_escape_string($link, $_REQUEST['yourname']);
+$username = mysqli_real_escape_string($link, $_REQUEST['username']);
+$email = mysqli_real_escape_string($link, $_REQUEST['email']);
+$password = mysqli_real_escape_string($link, $_REQUEST['password']);
+$repassword = mysqli_real_escape_string($link, $_REQUEST['repassword']);
+
+ 
