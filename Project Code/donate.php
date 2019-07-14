2f324476eed95fd30a7845835a9936e7601fc4d1
Irtifa <irtifa.sarwath@northsouth.edu>
2019-07-15 00:57:43 +0600

    Feature : creating registration form issue #4
    Issue #4

---
+++ Project Code/donate.php
@@ -0,0 +1,31 @@
+<?php
+
+$link = mysqli_connect("localhost", "root", "", "database");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+ // Escape user inputs for security
+$type = mysqli_real_escape_string($link, $_REQUEST['type']);
+$ccname = mysqli_real_escape_string($link, $_REQUEST['ccname']);
+$ccno = mysqli_real_escape_string($link, $_REQUEST['ccno']);
+$date = mysqli_real_escape_string($link, $_REQUEST['date']);
+$cvv = mysqli_real_escape_string($link, $_REQUEST['cvv']);
+
+// attempt insert query execution
+$sql = "INSERT INTO donate (type, ccname,ccno, date, cvv ) 
+VALUES ('$type', '$ccname','$ccno', '$date', '$cvv' )";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ // close connection
+mysqli_close($link);
+?>
+<html>
+    <br/><a href="donate.php">Another Donation<a>
+    <br/><a href="index.html">Go to homepage<a>
+</html>
