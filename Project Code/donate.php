a81b733a9da0f8ea8ec70c9a1d99696718891acc
Irtifa <irtifa.sarwath@northsouth.edu>
2019-07-15 00:38:56 +0600

    Feature : Creating donation form with php
    Issue #11

--- Project Code/donate.php
+++ Project Code/donate.php
@@ -13,3 +13,13 @@ $ccname = mysqli_real_escape_string($link, $_REQUEST['ccname']);
 $ccno = mysqli_real_escape_string($link, $_REQUEST['ccno']);
 $date = mysqli_real_escape_string($link, $_REQUEST['date']);
 $cvv = mysqli_real_escape_string($link, $_REQUEST['cvv']);
+
+// attempt insert query execution
+$sql = "INSERT INTO donate (type, ccname,ccno, date, cvv ) 
+VALUES ('$type', '$ccname','$ccno', '$date', '$cvv' )";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
