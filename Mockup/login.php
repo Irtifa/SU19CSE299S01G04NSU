f53d6ee8ae5bdd249cc5e924446ab847686ff206
Irtifa <irtifa.sarwath@northsouth.edu>
2019-07-28 21:19:45 +0600

    Feature #26: create a php file for log in with html

--- Mockup/login.php
+++ Mockup/login.php
@@ -4,6 +4,12 @@
 	<title>Login</title>
 </head>
 <body>
-
+<form action="" method="post">
+<label>Username:</label><input type="text" name="user"><br/>
+<label>Password:</label><input type="password" name="pass"><br/>
+<input type="submit" value="Login" name="submit"><br/>
+<!--New user Register Link -->
+<p><a href="register.php">New User Registeration!</a></p>
+</form>
 </body>
 </html>
