a9e4fe8e0c725d376f79742c84bab9e064f55e10
Irtifa <irtifa.sarwath@northsouth.edu>
2019-08-04 21:56:36 +0600

    Feature #18 :Create a registration with php  div part 2

--- Mockup/admin/registration.php
+++ Mockup/admin/registration.php
@@ -30,3 +30,11 @@
 <form name="registration" action="" method="post">
 <input type="text" name="username" placeholder="Orphanage Name" required />
 <input type="email" name="email" placeholder="Email" required />
+<input type="password" name="password" placeholder="Password" required />
+<input type="submit" name="submit" value="Register" />
+</form>
+
+</div>
+<?php } ?>
+</body>
+</html>
