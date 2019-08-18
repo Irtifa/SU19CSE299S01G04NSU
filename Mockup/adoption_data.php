e209c10f56bfb541b3da8a7b343e9a80105702d8
Irtifa <51863159+Irtifa@users.noreply.github.com>
2019-08-18 12:56:00 +0600

    Feature #39:create a php file for css connection

--- Mockup/adoption_data.php
+++ Mockup/adoption_data.php
@@ -1,3 +1,13 @@
 <?php
 $connection = mysqli_connect("localhost", "root", "root", "database");
 ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <title>Orphanage Hope</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="css/jquery.mobile-1.4.5.min.css" type="text/css">
