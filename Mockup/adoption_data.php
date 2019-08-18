5a33f6d16690ef548a9a37aa79cc8277d25eb0fe
Irtifa <51863159+Irtifa@users.noreply.github.com>
2019-08-18 13:01:31 +0600

    Feature #39:create a php file html part

--- Mockup/adoption_data.php
+++ Mockup/adoption_data.php
@@ -14,3 +14,11 @@ $connection = mysqli_connect("localhost", "root", "root", "database");
         <script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
         <script type="text/javascript" src="js/jquery.mobile-1.4.5.min.js"></script>
         <script type="text/javascript" src="js/script.js"></script>
+        <body>
+            <div class="main">
+                <div class="header">
+
+                    
+                    <div class="clr"></div>
+                    <div class="hbg"><img src="images/header_images.jpg" width="670" height="298" alt="" class="fl" />
+                        <div class="info fl">
