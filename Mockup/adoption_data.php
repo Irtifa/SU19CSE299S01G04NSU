096ec7b156660fc8400d3bd8412e3c2a8d91291b
Irtifa <51863159+Irtifa@users.noreply.github.com>
2019-08-18 13:00:02 +0600

    Feature #39:create a php file with j query files

--- Mockup/adoption_data.php
+++ Mockup/adoption_data.php
@@ -11,3 +11,6 @@ $connection = mysqli_connect("localhost", "root", "root", "database");
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link href="css/style.css" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="css/jquery.mobile-1.4.5.min.css" type="text/css">
+        <script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+        <script type="text/javascript" src="js/jquery.mobile-1.4.5.min.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
