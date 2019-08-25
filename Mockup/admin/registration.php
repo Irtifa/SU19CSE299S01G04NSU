474bb14866df2fb39028fe3480c14ff0d608cd69
Irtifa <51863159+Irtifa@users.noreply.github.com>
2019-08-25 22:30:33 +0600

    Feature #18 : Registration form final

--- Mockup/admin/registration.php
+++ Mockup/admin/registration.php
@@ -3,12 +3,13 @@
 <head>
 <meta charset="utf-8">
 <title>Registration</title>
-<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="css/x.css" />
+
 </head>
 <body>
 <?php
 	require('db.php');
-	// If form submitted, insert values into the database.
+    // If form submitted, insert values into the database.
     if (isset($_REQUEST['username'])){
 		$username = stripslashes($_REQUEST['username']); // removes backslashes
 		$username = mysqli_real_escape_string($con,$username); //escapes special characters in a string
@@ -23,12 +24,12 @@
         if($result){
             echo "<div class='form'><h3>You are registered successfully.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
         }
-        }else{
+    }else{
 ?>
 <div class="form">
 <h1>Registration</h1>
 <form name="registration" action="" method="post">
-<input type="text" name="username" placeholder="Orphanage Name" required />
+<input type="text" name="username" placeholder="Username" required />
 <input type="email" name="email" placeholder="Email" required />
 <input type="password" name="password" placeholder="Password" required />
 <input type="submit" name="submit" value="Register" />
@@ -36,5 +37,8 @@
 
 </div>
 <?php } ?>
+
+
 </body>
 </html>
+
