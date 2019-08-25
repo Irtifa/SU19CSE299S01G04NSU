70a15dd6359ec0b1b58ab3ea53042c487a0c9f8f
Irtifa <51863159+Irtifa@users.noreply.github.com>
2019-08-25 21:41:27 +0600

    Feature #44 : index for admin

---
+++ Mockup/admin/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Orphans</title>
+    <meta charset="UTF-8">
+    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="../css/jquery.mobile-1.4.5.min.css" type="text/css">
+
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+
+    <script type="text/javascript" src="../js/jquery-1.3.2.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.mobile-1.4.5.min.js"></script>
+    <script type="text/javascript" src="../js/script.js"></script>
+
+    <body>
+        <div class="main">
+            <div class="header">
+
+               <div class="clr"></div>
+                <div class="hbg"><img src="../images/or.jpg" width="1000" height="400" alt="" class="fl" /></div>
+                <div class="clr"></div>
+                    
+                    <!-- <div class="clr"></div> -->
+                </div>
+                <!-- <div class="clr"></div> -->
+            </div>
+            <div class="content">
+                <div class="mainbar">
+                    
+                    <div class="article">
+                        <h2><span>Welcome to your Dashboard</span> </h2>
+
+                     
+                        <a href="../donate_data.php"><button  class="ui-btn ui-shadow">Donation From Donor</button></a>
+                        <a href="../adoption_data.php"><button  class="ui-btn ui-shadow">Adoption Form's Response</button></a>
+                        
+                        <br>
+                        <br>
+                        <br>
+                        <a href="../index.html"><button  class="ui-btn ui-shadow">Back To Main Site</button></a>
+                        <a href="logout.php"><button  class="ui-btn ui-shadow">Log Out</button></a>
+
+                    </div>
+
+                </div>
+                
+                <div class="clr"></div>
+            </div>
+            
+            <div class="footer">
+                <div class="footer_resize">
+                    <p class="lf">&copy; Copyright <a href="#">Orphans>All right reserved.</p>
+
+                    <div class="clr"></div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
